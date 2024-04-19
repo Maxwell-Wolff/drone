7fe2fdd0a011f7d97cf9a98bd67b3df4e9a6ba55
Maxwell Wolff <89609459+Maxwell-Wolff@users.noreply.github.com>
2024-04-19 02:42:09 -0400

    Update ArucoTemplate.py

--- ArucoTemplate.py
+++ ArucoTemplate.py
@@ -1,5 +1,3 @@
-91% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for $1.99 $0.49 for 1 month.
-#!/usr/bin/env python
 
 ########IMORTS#########
 
