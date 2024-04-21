526a64a07a96b83c08cd5438e8bedb36142fd204
Maxwell Wolff <89609459+Maxwell-Wolff@users.noreply.github.com>
2024-04-21 04:16:06 -0400

    Update throttleup.py

--- throttleup.py
+++ throttleup.py
@@ -41,9 +41,16 @@ if __name__ == "__main__":
 		vehicle = connectMyCopter()
 		arm()
 		vehicle.channels.overrides['3'] = 0
-		time.sleep(0.5)
-		vehicle.channels.overrides['3'] = 0
+		#time.sleep(0.5)
+		#vehicle.channels.overrides['3'] = 0
 		time.sleep(1)
+		#vehicle.channels.overrrides['3']=0
+		vehicle.channels.overrides['3'] = 1350
+		time.sleep(1)
+ 		vehicle.channels.overrides['3'] = 1500
+ 		time.sleep(1)
+ 		# vehicle.channels.overrides['3'] = 0
+ 		# time.sleep(1)
 		msg = vehicle.message_factory.command_long_encode(
 			0,0, #target system, target component
 			common.MAV_CMD_DO_FLIGHTTERMINATION, #command
@@ -52,13 +59,6 @@ if __name__ == "__main__":
 			0,0,0, #unused params
 			0,0,0)
 		vehicle.send_mavlink(msg)
-		#vehicle.channels.overrrides['3']=0
-		#vehicle.channels.overrides['3'] = 1500
-		#time.sleep(2)
- 		# vehicle.channels.overrides['3'] = 1650
- 		# time.sleep(2)
- 		# vehicle.channels.overrides['3'] = 0
- 		# time.sleep(1)
 		vehicle.close
 		sys.exit()
 
