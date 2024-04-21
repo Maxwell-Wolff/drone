f8f443e6f2ddab1dac8ba3425c93b114bc1045be
Maxwell Wolff <89609459+Maxwell-Wolff@users.noreply.github.com>
2024-04-21 03:49:55 -0400

    Create throttleup.py

---
+++ throttleup.py
@@ -0,0 +1,54 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import time
+import socket
+#import exceptions
+import math
+import argparse
+import os
+import sys
+
+def connectMyCopter():
+        parser = argparse.ArgumentParser(description='commands')
+        parser.add_argument('--connect')
+        args = parser.parse_args()
+
+        connection_string = args.connect
+        baud_rate = 57600
+
+        vehicle = connect(connection_string,baud=baud_rate) #,wait_ready=True)
+        return vehicle
+
+def arm():
+        while vehicle.is_armable==False:
+                print("Waiting for vehicle to become armable..")
+                time.sleep(1)
+        print("Vehicle is now armable")
+        print("")
+
+        vehicle.armed=True
+        while vehicle.armed==False:
+                print("Waiting for drone to become armed..")
+                time.sleep(1)
+
+        print("Vehicle is now armed")
+        print("Props are spinning!!")
+
+        return None
+
+if __name__ == "__main__"
+
+  vehicle = connectMyCopter()
+  arm()
+  vehicle.channels.overrides['3'] = 0
+  time.sleep(2)
+  vehicle.channels.overrides['3'] = 1350
+  time.sleep(2)
+  vehicle.channels.overrides['3'] = 1500
+  time.sleep(2)
+  vehicle.channels.overrides['3'] = 1650
+  time.sleep(2)
+  vehicle.channels.overrides['3'] = 0
+  time.sleep(1)
+  vehicle.close
+  sys.exit()
+
